91f110d64c86a75864637c83218209228f43966b
RaccoonskY <viktoryablonskas@yandex.ru>
2023-11-02 19:00:26 +0300

    lab3 done

---
+++ lab3_forms/AddFileDiaglogForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace lab3_forms
+{
+    partial class AddFileDiaglogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.filenameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.Location = new System.Drawing.Point(85, 109);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(92, 36);
+            this.OkBtn.TabIndex = 0;
+            this.OkBtn.Text = "Ok";
+            this.OkBtn.UseVisualStyleBackColor = true;
+            this.OkBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(244, 109);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(92, 36);
+            this.CancelBtn.TabIndex = 1;
+            this.CancelBtn.Text = "Назад";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // filenameTextBox
+            // 
+            this.filenameTextBox.Location = new System.Drawing.Point(85, 81);
+            this.filenameTextBox.Name = "filenameTextBox";
+            this.filenameTextBox.Size = new System.Drawing.Size(251, 22);
+            this.filenameTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите название и формат файла:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // AddFileDiaglogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 186);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filenameTextBox);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.OkBtn);
+            this.Name = "AddFileDiaglogForm";
+            this.Text = "AddFilecs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OkBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.TextBox filenameTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ lab3_forms/MainForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WorksWithFilesLibrary;
+
+namespace lab3_forms
+{
+    public partial class MainForm : Form
+    {
+
+        private readonly WorkWithFiles _fileOperator;
+        private string _rootDir;
+        private string _curFileChosen;
+        public MainForm(string rootdir)
+        {
+            _rootDir = rootdir;
+            _fileOperator = new WorkWithFiles(rootdir);
+            InitializeComponent();
+            this.CurrentDirLbl.Text = rootdir;
+            UpdateListOfFiles();
+        }
+
+        private void UpdateListOfFiles()
+        {
+            this.FilesListBox.Items.Clear();
+            this.FilesListBox.Items.AddRange(_fileOperator.GetAllFiles());
+        }
+
+        private void AddFile(string filename)
+        {
+            var res = _fileOperator.CreateFile(filename);
+            if (res == -2)
+            {
+                MessageBox.Show($"{filename} - неправильное расширение файла!");
+            }
+            else if (res == -1)
+            {
+                MessageBox.Show("Произошла ошибка при создании файла");
+            }
+            else
+            {
+                MessageBox.Show("Файл успешно создан");
+                UpdateListOfFiles();
+            }
+        }
+
+        private void ChangeDirBySelected()
+        {
+            var selectedItem = FilesListBox.SelectedItem;
+            if (selectedItem != null && Directory.Exists(selectedItem.ToString()))
+            {
+
+                try
+                {
+                    _fileOperator.ChangeDirectory(selectedItem.ToString());
+                    this.CurrentDirLbl.Text = selectedItem.ToString();
+                    UpdateListOfFiles();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла непредвиденная ошибка\nПопробуйте перезапустить программу\nОшибка: {ex.Message}");
+                }
+            }
+        }
+
+        private void DeleteFile()
+        {
+            var selectedItem = FilesListBox.SelectedItem;
+            var res = _fileOperator.DeleteFile(selectedItem.ToString());
+            if (res == -2)
+            {
+                MessageBox.Show($"{selectedItem} - такого файла не существует");
+            }
+            else if (res == -1)
+            {
+                MessageBox.Show("Произошла ошибка при удалении файла");
+            }
+            else
+            {
+                MessageBox.Show("Файл успешно удалён");
+                UpdateListOfFiles();
+            }
+
+        }
+
+
+        private void GoBackDirBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _fileOperator.ChangeDirectory("..");
+                this.CurrentDirLbl.Text = _fileOperator.CurrentDirectory;
+                UpdateListOfFiles();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show($"Произошла непредвиденная ошибка\nПопробуйте перезапустить программу\nОшибка: {ex.Message}");
+            }
+        }
+
+        private void ShowDirBtn_Click(object sender, EventArgs e)
+        {
+           UpdateListOfFiles();
+        }
+
+        private void ChangeDirBtn_Click(object sender, EventArgs e)
+        {
+            ChangeDirBySelected();
+        }
+
+        private void FilesListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ChangeDirBySelected();
+        }
+
+        private void AddFileBtn_Click(object sender, EventArgs e)
+        {
+            var addfile =  new AddFileDiaglogForm(AddFile);
+            addfile.ShowDialog();
+        }
+
+        private void DeleteFileBtn_Click(object sender, EventArgs e)
+        {
+            DeleteFile();
+        }
+    }
+}
